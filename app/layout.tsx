50c3177fbf81375bdb0d88c5b1d2ad6ac9cb51b1
thejustinson <joelnwachukwu67@gmail.com>
2025-07-09 15:43:23 +0100

    fixing app height

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#FBF5D7" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="default"/>
       </head>
       <body
         className={`${geologica.variable} antialiased`}
